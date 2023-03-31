7c98a34f90fa21a7a5edeaaa3c7eac76ad2ca18c
Thuy Nguyen <vanthuy101102@gmail.com>
2023-03-31 11:31:07 +0700

    fix bug something

--- assets/Javascript/main.js
+++ assets/Javascript/main.js
@@ -1,11 +1,20 @@
 let liked = document.querySelector(".liked");
 
-liked.onclick = function(){
-    if(liked.classList.contains("liked")){
-        liked.classList.remove("liked")
-        //nếu có class liked thì bỏ nó đi
-    }else{
-        liked.classList.add("liked");
-        //không có thì add class đó vào
-    }
+liked.onclick = function () {
+  if (liked.classList.contains("liked")) {
+    liked.classList.remove("liked");
+    //nếu có class liked thì bỏ nó đi
+  } else {
+    liked.classList.add("liked");
+    //không có thì add class đó vào
+  }
+};
+
+let cart = document.querySelector(".cart__wrap");
+let cartList = document.querySelector(".cart__list");
+let cartItem = document.querySelectorAll(".cart__item-add");
+let cartNum = document.querySelector(".cart__noti");
+cartNum.textContent = cartItem.length;
+cart.onclick = () => {
+  cartList.classList.toggle("show");
 };
